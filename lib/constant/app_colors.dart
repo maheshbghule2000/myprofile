939b2ee17f27b99884c1ef70968ee862c6476b39
maheshbghule2000 <maheshbghule2000@gmail.com>
2025-01-21 18:27:16 +0530

    Add font assets and update theme configuration in main app

---
+++ lib/constant/app_colors.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color kPrimaryColor = Color(0xFFFFFDFE);
+  static const Color kcScaffoldBackgroundColor = Color(0xFFFFFFFF);
+  static const Color kPrimaryBoldColor = Color(0xFF167C84);
+  static const Color kPrimaryLight = Color(0xFF167C84);
+  static const Color kBottonColor = Color(0xFF00AFAA);
+  static const Color kBorderColor = Color(0xFFD9D8D6);
+  static const Color kBgColor = Color(0xFFFBFBFB);
+  static const Color kcardColor = Color(0xFFEFEFEF);
+  static const Color kcDarkGray3Color = Color(0xFF54565a);
+  static const Color kfaintGrayColor = Color(0xFFE5E4E4);
+  static const Color kcDarkGray4Color = Color(0xFF54565A);
+  static const Color kPrimaryFaintGreenColor = Color(0xFF41A757);
+  static const Color kConditionalApprovedColor = Color(0xFF3bd4ae);
+  static const Color kinActiveColor = Color(0xFFcddb00);
+  static const Color kOrangeColor = Color(0xFFff8300); // Orange
+  static const Color kWhite = Color(0xffffffff);
+  static const Color kAquaGreen = Color.fromARGB(255, 56, 77, 76); // Aqua Green
+  static const Color kFentGrey = Color(0xFFD9D8D6); // Grey
+
+  static const Color kSecondaryLight = Color(0xFF9C9C9C);
+  static const Color kError = Color(0xFFB00020);
+  static const Color kRatingIconColor = Color(0xFFFFC319);
+  static const Color kcDarkGray2Color = Color(0xff303633);
+
+  static const Color kcPrimaryGreenColor = Color(0xff56A145);
+  static const Color kcPrimaryLightGreenColor = Color(0xffabd0a2);
+  static const Color kcPrimaryLightGreenColor2 = Color(0xffDBFFD9);
+  static const Color kcPremiumStarColorBackground = Color(0xffCDFFC2);
+  static const Color kcPrimaryDarkGreenColor = Color(0xff2D721D);
+  static const Color kcPrimaryOrangeColor = Color(0xffFF941F);
+  static const Color kcPrimaryDarkOrangeColor = Color(0xffE97A00);
+  static const Color kcPrimaryLightOrangeColor = Color(0xffFFFDEC);
+  static const Color kcExpenseColor = Color(0xffE65025);
+  static const Color kcScaffoldBackgroundColor2 = Color(0xffF8F8F8);
+  static const Color kcDarkRed = Color(0xff5D1919);
+  static const Color kcLightGreen = Color(0xff006B63);
+  static const Color kcLight2Green = Color(0xff7BE376);
+  static const Color kExpansionTileBg = Color(0xFFF1FCFF);
+  static Color kLightGreyText = const Color(0xFF2A2A2A).withOpacity(0.8);
+}
